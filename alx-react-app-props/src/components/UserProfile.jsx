335237b719d05c59d0b3a5e6aaa897b9eb3ec6ff
Mutwingima Mariam <princessmirina45@gmail.com>
2025-11-07 05:57:33 +0200

    Update App.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,7 +1,23 @@
 import UserInfo from "./UserInfo";
+import { useContext } from "react";
+import UserContext from "../UserContext";
 
 function UserProfile() {
-  return <UserInfo />;
+  // Access user data from context
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>
+        <strong>Name:</strong> {userData.name}
+      </p>
+      <p>
+        <strong>Email:</strong> {userData.email}
+      </p>
+      <UserInfo />
+    </div>
+  );
 }
 
 export default UserProfile;
