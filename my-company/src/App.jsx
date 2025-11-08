c85adad2d65d137e20fe8faa4dccff040211cf4a
Mutwingima Mariam <princessmirina45@gmail.com>
2025-11-08 05:02:59 +0200

    Content and Component Structure

---
+++ my-company/src/App.jsx
@@ -0,0 +1,14 @@
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Home />
+        <About />
+      </div>
+    </>
+  );
+}
+
+export default App;
