40ad9e7d0929529802fe8795e950b75df1d3fc59
Mutwingima Mariam <princessmirina45@gmail.com>
2025-11-08 05:29:36 +0200

    Update App.jsx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,17 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
+
 function App() {
   return (
-    <>
-      <div>
-        <Home />
-        <About />
-        <Services />
-        <Contact />
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
