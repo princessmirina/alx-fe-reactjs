80fe8e6af549e5fb03f42ad404432f4e3bd6da08
Mutwingima Mariam <princessmirina45@gmail.com>
2025-11-08 05:13:48 +0200

    Component Structure

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,11 +1,15 @@
 import Home from "./components/Home";
-
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
 function App() {
   return (
     <>
       <div>
         <Home />
         <About />
+        <Services />
+        <Contact />
       </div>
     </>
   );
