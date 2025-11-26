893b054d09249250bc9c30d103dc1f4978d02ae4
Mutwingima Mariam <princessmirina45@gmail.com>
2025-11-26 04:18:38 +0200

    Fix file

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 const UserProfile = () => {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 shadow-lg">
+    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img
         src="https://placeholder.co/150x150"
         alt="User"
